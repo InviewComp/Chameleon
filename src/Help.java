817d263779e92329fb59e1d15f28dd86053e7b38
InviewCompany <mr_a953@mail.ru>
2018-04-15 11:13:04 +0300

    Проект Хамелеон 0.1

--- src/Help.java
+++ src/Help.java
@@ -6,6 +6,11 @@ public class Help {
     Help(){
         out.println(ANSI_YELLOW+"HELP");
       out.println("/help-вывод списка команд");
+        out.println("/enc-запуск машины");
+        out.println("В командах шифра:\n" +
+                "-e:выход из этого шифра \n" +
+                "-s:запуск шифрования \n" +
+                "-h:обьяснение шифра \n");
       out.println("/exit - выход из консоли"+ANSI_GREEN);
     }
 }
